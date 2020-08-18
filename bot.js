56865e20a1202309507a3aa50d49e83fb09789a4
imrubennnn <69871707+imrubennnn@users.noreply.github.com>
2020-08-18 23:51:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,44 @@
-const Discord = require("discord.js") ;const client = new Discord.client();
-const config = require("./config.json");
-client.on("ready", () => {
-    console.log(Bot foi iniciado, com $ {
-            client.users.size
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json") client.on("ready", () => {
+    console.log(O bot foi iniciado, com $ {
+            client.users.cache.size
         }
-        usuários, em $ {
-            client.channels.size
-        }
-        canais, em $ {
-            client.guilds.size
+        usuários e em $ {
+            client.guilds.cache.size
         }
         servidores.);
-    client.uiser.setGames(Eu entou em $ {
-            client.guilds.size
+    client.user.setActivity(em $ {
+            client.guilds.cache.size
         }
         servidores);
 });
 client.on("guildCreate", guild => {
-    console.log(0 bot entrou no servidor: $ {
+    console.log(O bot entrou no servidor: $ {
             guild.name
+        }(ID do servidor: $ {
+            guild.id
+        }).Membros: $ {
+            guild.memberCount
+        }
+        membros!);
+    client.user.setActivity(Estou em $ {
+            client.guilds.cache.size
+        }
+        servidores.);
+});
+client.on("guildDelete", guild => {
+    console.log(O bot foi removido do servidor: $ {
+        guild.name
+    }(ID do servidor: $ {
+        guild.id
+    }));
+    client.user.setActivity(Serving $ {
+            client.guilds.cache.size
+        }
+        servers);
+});
+client.on("message", async message => {
+                if (message.author.bot) return;
+                if (message.channel.type === "dm") return;
+                const args = message.content.slice(config.prefix.length).trim().sp
